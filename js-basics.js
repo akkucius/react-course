9160374f4624b79aa0c306b3c08d560fc8bce7d1
MEDIA\ankur.sr <ankur.sr@media.net>
2023-05-02 22:04:19 +0530

    JS Basics for Classes and Constructor with Let and Constant

---
+++ js-basics.js
@@ -0,0 +1,26 @@
+class Human {
+  gender = 'male';
+  
+  printGender = () => {
+    console.log(this.gender);
+  }
+}
+
+
+class Person extends Human{
+  
+  name = 'Max';
+  gender = 'female';
+  
+  printName = () => {
+    console.log(this.name)
+  }
+  
+   
+}
+
+const person = new Person();
+
+person.printName();
+
+person.printGender();
